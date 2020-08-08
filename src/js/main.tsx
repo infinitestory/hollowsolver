96dd0a3b85eb03d60a3e3f8b6920ec502f2230a0
infinitestory <infinitestory@users.noreply.github.com>
2020-08-08 01:33:08 -0700

    bad ui

--- src/js/main.tsx
+++ src/js/main.tsx
@@ -1,13 +1,98 @@
+import * as React from "react";
+import { render } from "react-dom";
+
 import cloneDeep from "lodash-es/cloneDeep";
 import maxBy from "lodash-es/maxBy";
 import range from "lodash-es/range";
+
+interface GridState {
+  grid: boolean[][];
+}
+
+interface GridProps {
+
+}
+
+class Grid extends React.Component<GridProps, GridState> {
+  constructor(props: GridProps) {
+    super(props);
+    this.state = {
+      grid: [
+  [false, false, false, false, false, false],
+  [false, false, true, false, false, false],
+  [false, false, false, false, true, false],
+  [false, false, false, false, false, true],
+  [false, true, false, false, false, false],
+  [false, false, false, true, false, false],
+],
+    };
+  }
+
+  toggleCell = (coords: Coordinates) => {
+    console.log(coords);
+    this.setState((state) => {
+      let newState = cloneDeep(state);
+      newState.grid[coords.y][coords.x] = !newState.grid[coords.y][coords.x];
+      return newState;
+    });
+  }
+
+  render() {
+    const targetCoordinates = SolveGrid(this.state.grid);
+
+    const wrapperStyle = {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(6, 1fr)',
+      gridTemplateRows: 'repeat(6, 1fr)',
+      width: '360px',
+      height: '360px',
+    }
+
+    return (
+      <div style={wrapperStyle}>{
+        range(36).map(v => {
+          const coords = convertToCoordinates(v, false);
+          const gridSquareProps = {
+            coordinates: coords,
+            isSelected: this.state.grid[coords.y][coords.x],
+            isTarget: targetCoordinates.x == coords.x && targetCoordinates.y == coords.y,
+            clickHandler: () => {this.toggleCell(coords)},
+          }
+          return <GridSquare {...gridSquareProps} key={`grid-${coords.x}-${coords.y}`}/>
+        })
+      }</div>
+    );
+  }
+}
+
+interface GridSquareProps {
+  coordinates: Coordinates;
+  isSelected: boolean;
+  isTarget: boolean;
+  clickHandler: (e: React.MouseEvent) => void;
+}
+
+const GridSquare = (props: GridSquareProps) => {
+  const style = {
+    backgroundColor: props.isSelected? 'blue' : 'green',
+    border: props.isTarget? '2px dashed red' : 'none',
+    gridColumn: props.coordinates.x + 1,
+    gridRow: props.coordinates.y + 1,
+  }
+
+  return (
+    <div style={style} onClick={props.clickHandler}>
+    </div>
+  );
+}
+
 
 interface Coordinates {
   x: number;
   y: number;
 }
 
-export const SolveGrid = (grid: Boolean[][]) => {
+const SolveGrid = (grid: boolean[][]) => {
   // Compute the coordinates of all open 2x3 and 3x2 rectangles as-is.
   const openWideCoordSet = getOpenCoordinates(3, 2, grid);
   const openTallCoordSet = getOpenCoordinates(2, 3, grid);
@@ -24,21 +109,21 @@ export const SolveGrid = (grid: Boolean[][]) => {
     for (let j = i+1; j < 15; j++) {
       for (let k = j+1; k < 16; k++) {
         let filledGrid = cloneDeep(grid);
-        const ci = convertToCoordinates(i);
+        const ci = convertToCoordinates(i, true);
         if (filledGrid[ci.y][ci.x]) {
           continue;
         } else {
           filledGrid[ci.y][ci.x] = true;
         }
 
-        const cj = convertToCoordinates(j);
+        const cj = convertToCoordinates(j, true);
         if (filledGrid[cj.y][cj.x]) {
           continue;
         } else {
           filledGrid[cj.y][cj.x] = true;
         }
 
-        const ck = convertToCoordinates(k);
+        const ck = convertToCoordinates(k, true);
         if (filledGrid[ck.y][ck.x]) {
           continue;
         } else {
@@ -60,18 +145,18 @@ export const SolveGrid = (grid: Boolean[][]) => {
 
   return convertToCoordinates(maxBy(bestIndices, v => {
     let filledGrid = cloneDeep(grid);
-    const cv = convertToCoordinates(v);
+    const cv = convertToCoordinates(v, true);
     filledGrid[cv.y][cv.x] = true;
 
     const remainingWideOpen = getOpenCoordinates(3, 2, filledGrid, openWideCoordSet);
     const remainingTallOpen = getOpenCoordinates(2, 3, filledGrid, openTallCoordSet);
     return -(remainingTallOpen.length + remainingWideOpen.length);
-  }));
+  }), true);
 }
 
 // Grid input is True for blocked squares, False for non-blocked squares
 // Returns a set of coordinate pairs which correspond to the upper right of rectangles that are fully available.
-const getOpenCoordinates = (width: number, height: number, grid: Boolean[][], coordSet?: Coordinates[]) => {
+const getOpenCoordinates = (width: number, height: number, grid: boolean[][], coordSet?: Coordinates[]) => {
   const gridWidth = grid[0].length;
   const gridHeight = grid.length;
   if (!coordSet) {
@@ -86,7 +171,7 @@ const getOpenCoordinates = (width: number, height: number, grid: Boolean[][], co
 }
 
 // Check whether an entire rectangle is open on the grid.
-const checkRectangleOpen = (width: number, height: number, grid: Boolean[][], upperLeft: Coordinates) => {
+const checkRectangleOpen = (width: number, height: number, grid: boolean[][], upperLeft: Coordinates) => {
   for (let x = upperLeft.x; x < upperLeft.x + width; x++) {
     for (let y = upperLeft.y; y < upperLeft.y + height; y++) {
       if (grid[x][y]) {
@@ -98,19 +183,14 @@ const checkRectangleOpen = (width: number, height: number, grid: Boolean[][], up
 }
 
 // Convert a number (0-15) to x,y coordinates of a square in the center 4x4.
-const convertToCoordinates = (openSquareIndex: number) => {
+const convertToCoordinates = (openSquareIndex: number, pad: boolean) => {
   return {
-    x: (openSquareIndex % 4) + 1,
-    y: Math.floor(openSquareIndex / 4) + 1,
+    x: (openSquareIndex % (pad ? 4 : 6)) + (pad ? 1 : 0),
+    y: Math.floor(openSquareIndex / (pad ? 4 : 6)) + (pad ? 1 : 0),
   } as Coordinates;
 }
-/*
-console.log(SolveGrid([
-  [false, false, false, false, false, false],
-  [false, false, true, false, false, false],
-  [false, false, false, false, true, false],
-  [false, false, false, false, false, true],
-  [false, true, false, false, false, false],
-  [false, false, false, true, false, false],
-]))
-*/
+
+render(
+  <Grid />,
+  document.getElementById("root"),
+);
